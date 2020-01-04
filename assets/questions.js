68caaafb63513fd514e0f0ed05217eb7df61c7af
XikkeN <e.humphreys@tricentis.com>
2020-01-04 18:22:13 -0500

    Finishing up scripting and CSS tweaks

--- assets/questions.js
+++ assets/questions.js
@@ -1,114 +1,151 @@
 //DOM elements
-const start = document.getElementById("start");
+const start = document.getElementById("startButton");
 const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
+const quizTimer = document.getElementById("quizTimer");
+const questionText = document.getElementById("questionText");
 const qImg = document.getElementById("questionImage");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
+const choices = document.getElementById("choices");
 const counter = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
+const scoreNumber = document.getElementById("scoreNumber");
+const initialsInput = document.getElementById("initialsInput");
+const scoreValue = document.getElementById("scoreValue");
+const highScores = document.getElementById("highScores");
+const scoreEntry = document.getElementById("scoreEntry");
+const leaderboard = document.getElementById("leaderboardButton");
 
 //Array containing my questions and data
 let questions = [
     {
-        question: "Commonly used data types DO NOT include:",
-        choiceA: "alerts",
-        choiceB: "strings",
-        choiceC: "numbers",
-        correct: "A",
+        questionText: "Commonly used data types DO NOT include:",
+        choices: ["alerts", "strings", "numbers"],
+        correctIndex: 0,
         imgSrc: "assets/js.png"
     },
     {
-        question: "The condition in an if / else statement is enclosed within ____.",
-        choiceA: "quotes",
-        choiceB: "parentheses",
-        choiceC: "curly brackets",
-        correct: "B",
+        questionText: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "parentheses", "curly brackets"],
+        correctIndex: 1,
         imgSrc: "assets/js.png"
     },
     {
-        question: "Javascript and Java are basically the same?",
-        choiceA: "True",
-        choiceB: "False",
-        choiceC: "Maybe?",
-        correct: "B",
+        questionText: "Javascript and Java are basically the same?",
+        choices: ["True", "False", "Maybe?"],
+        correctIndex: 1,
         imgSrc: "assets/js.png"
     },
     {
-        question: "Which HTML tag is used to define an internal style sheet?",
-        choiceA: "&#60;style&#62;",
-        choiceB: "&#60;script&#62;",
-        choiceC: "&#60;css&#62;",
-        correct: "A",
+        questionText: "Which HTML tag is used to define an internal style sheet?",
+        choices: ["&#60;style&#62;", "&#60;script&#62;", "&#60;css&#62;"],
+        correctIndex: 0,
         imgSrc: "assets/css.png"
     },
     {
-        question: "Which element has the largest heading?",
-        choiceA: "&#60;H1&#62;",
-        choiceB: "&#60;head&#62",
-        choiceC: "&#60;h6&#62",
-        correct: "B",
+        questionText: "Which element has the largest heading?",
+        choices: ["&#60;H1&#62;", "&#60;head&#62", "&#60;h6&#62"],
+        correctIndex: 1,
         imgSrc: "assets/html.png"
-    },
+    }
 ];
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
+
+const lastQuestionIndex = questions.length - 1;
+let currentQuestionIndex = 0;
+let questionTimeCounter = 10;
+let quizTimeCounter = 75;
+let correctAnswerCount = 0;
+const counterWidth = 150; // 150px
+const counterIncrement = counterWidth / questionTimeCounter;
 let TIMER;
 let score = 0;
 
 // render a question
 function renderQuestion(){
-    let q = questions[runningQuestion];
-    
-    question.innerHTML = "<p>"+ q.question +"</p>";
-    qImg.innerHTML = "<img src="+ q.imgSrc +">";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
+    questionText.innerHTML = "";
+    qImg.innerHTML = "";
+    choices.innerHTML = "";
+    questionTimeCounter = 10;
+    let q = questions[currentQuestionIndex];
+    let text = document.createElement("p");   
+    text.innerHTML = q.questionText;
+    questionText.appendChild(text);
+    const questionImage = document.createElement("img");
+    questionImage.src = q.imgSrc;
+    qImg.appendChild(questionImage);
+
+    q.choices.forEach(function(choice, index) {
+        let questionChoice = document.createElement("button");
+        questionChoice.className = "btn btn-info choice";
+        questionChoice.onclick = function(e) {
+            checkAnswer(e);
+        };
+        questionChoice.value = index;
+        questionChoice.id = choice + "-" + index;
+        questionChoice.innerHTML = choice;
+        choices.appendChild(questionChoice);
+    })
 }
 
 start.addEventListener("click",startQuiz);
+leaderboard.addEventListener("click",renderLeaderboard);
+
+function renderLeaderboard() {
+    scoreDiv.style.display = "block";
+    scoreEntry.style.display = "none";
+    renderHighScores();
+}
 
 // start quiz
 function startQuiz(){
     start.style.display = "none";
-    leaderboard.style.display = "none";
+    leaderboardButton.style.display = "none";
+    scoreDiv.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
     renderProgress();
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    renderTimers();
+    TIMER = setInterval(renderTimers, 1000); // 1000ms = 1s
+}
+
+function renderTimers() {
+    renderQuizTimer();
+    renderQuestionTimer();
 }
 
 // render progress
 function renderProgress(){
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    for(let qIndex = 0; qIndex <= lastQuestionIndex; qIndex++){
+        let progressIndicator = document.createElement("div");
+        progressIndicator.className = "prog";
+        progressIndicator.id = "progressIndicator-" + qIndex;
+        progress.appendChild(progressIndicator);
     }
 }
 
-// counter render
+function renderQuizTimer() {
+    if (quizTimeCounter > 0) {
+        quizTimer.innerHTML = quizTimeCounter;
+        quizTimeCounter--
+    } else {
+        // TODO: Implement End Game
+    }
+}
 
-function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    }else{
-        count = 0;
+// render counter
+
+function renderQuestionTimer(){
+    if (questionTimeCounter > 0) {
+        counter.innerHTML = questionTimeCounter;
+        timeGauge.style.width = questionTimeCounter * counterIncrement + "px";
+        questionTimeCounter--
+    } else {
         // change progress color to red
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
+        setAnswerIncorrect();
+        if (currentQuestionIndex < lastQuestionIndex) {
+            currentQuestionIndex++;
             renderQuestion();
-        }else{
+        } else {
             // end the quiz and show the score
             clearInterval(TIMER);
             scoreRender();
@@ -119,55 +156,87 @@ function renderCounter(){
 // checkAnwer
 
 function checkAnswer(answer){
-    if( answer == questions[runningQuestion].correct){
-        // answer is correct
-        score++;
-        // change progress color to green
-        answerIsCorrect();
-    }else{
-        // answer is wrong
-        // change progress color to red
-        answerIsWrong();
+    const selectedAnswerIndex = parseInt(answer.target.value);
+    if ( selectedAnswerIndex === questions[currentQuestionIndex].correctIndex) {
+        setAnswerCorrect();
+    } else {
+        setAnswerIncorrect();
     }
-    count = 0;
-    if(runningQuestion < lastQuestion){
-        runningQuestion++;
+
+    if (currentQuestionIndex < lastQuestionIndex) {
+        currentQuestionIndex++;
         renderQuestion();
-    }else{
+    } else {
         // end the quiz and show the score
         clearInterval(TIMER);
         scoreRender();
     }
 }
 
-// answer is correct
-function answerIsCorrect(){
-    document.getElementById(runningQuestion).style.backgroundColor = "##F6AC72";
+function setAnswerCorrect() {
+    // increment score
+    score++
+    correctAnswerCount++;
+    // set indicator to green
+    setAnswerIndicatorColor("#77dd77");
+    // add 10 seconds to timer
+    updateTimer(10);
 }
 
-// answer is Wrong
-function answerIsWrong(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#ff6961";
+function setAnswerIncorrect() {
+    // set indicator to red
+    setAnswerIndicatorColor("#ff6961");
+    // subtract 10 seconds to timer
+    updateTimer(-10);
+}
+
+// answer is correct
+function setAnswerIndicatorColor(color){
+    const answerIndicatorId = "progressIndicator-" + currentQuestionIndex;
+    document.getElementById(answerIndicatorId).style.backgroundColor = color; 
+}
+
+function updateTimer(seconds) {
+    quizTimeCounter += seconds;
+    renderQuizTimer();
 }
 
 // score render
 function scoreRender(){
+    quiz.style.display = "none";
     scoreDiv.style.display = "block";
     
     // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score/questions.length);
-    
-    // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-              (scorePerCent >= 60) ? "img/4.png" :
-              (scorePerCent >= 40) ? "img/3.png" :
-              (scorePerCent >= 20) ? "img/2.png" :
-              "img/1.png";
-    
-    scoreDiv.innerHTML = "<img src="+ img +">";
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+    const score = correctAnswerCount * quizTimeCounter;
+
+    scoreNumber.innerHTML = score;
+    scoreValue.value = score;
+    renderHighScores();
 }
-// Leaderboard Modal
-$('#myModal').click('leaderboard', function () {
-    $('#myInput').trigger('focus')
-  })
+
+function saveInitials() {
+ const highScoresList = localStorage.getItem("highScores");
+ parsedHighScores = highScoresList ? JSON.parse(localStorage.getItem("highScores")) : [];
+
+ let newHighScore = {
+     initials: initialsInput.value,
+     score: scoreValue.value
+ };
+ parsedHighScores.push(newHighScore);
+
+ localStorage.setItem("highScores", JSON.stringify(parsedHighScores));
+ renderHighScores()
+}
+
+function renderHighScores() {
+    const highScoresList = localStorage.getItem("highScores");
+    parsedHighScores = highScoresList ? JSON.parse(localStorage.getItem("highScores")) : [];
+
+    highScores.innerHTML = "";
+
+    parsedHighScores.forEach(function(scoreItem) {
+        let listItem = document.createElement("li");
+        listItem.innerHTML = scoreItem.initials + " - " + scoreItem.score;
+        highScores.appendChild(listItem);
+    })
+}
